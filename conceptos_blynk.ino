362fbf1d4dd5be765572ab106ddb1b2bcd257b72
cristianovsky <cristianovsky@gmail.com>
2022-08-06 17:31:44 -0500

    first commit

---
+++ conceptos_blynk.ino
@@ -0,0 +1,84 @@
+/*************************************************************
+
+  This sketch shows how to write values to Virtual Pins
+
+  NOTE:
+  BlynkTimer provides SimpleTimer functionality:
+    http://playground.arduino.cc/Code/SimpleTimer
+
+  App project setup:
+    Value Display widget attached to Virtual Pin V5
+ *************************************************************/
+
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
+#define BLYNK_TEMPLATE_ID "TMPLrnEqXdIF"
+#define BLYNK_DEVICE_NAME "TEST3000"
+#define BLYNK_AUTH_TOKEN "qyPbAlD_6Sgd7YQrukif0mvAuQl33Qd8"
+
+
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "ssalome";
+char pass[] = "P0mp1l14";
+
+BlynkTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, analogRead(A0));
+}
+
+BLYNK_WRITE(V1)
+{
+  String pinValue = param.asStr(); // assigning incoming value from pin V1 to a variable
+  Serial.println(pinValue);
+  if(pinValue == "pass"){
+    Serial.println("Eureka ese es el password");
+    }
+}
+
+BLYNK_WRITE(V2)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+  if(pinValue == 1){
+    digitalWrite(2,HIGH);
+    }else{digitalWrite(2,LOW);}
+  
+}
+
+
+void setup()
+{
+  pinMode(2,OUTPUT);
+  // Debug console
+  Serial.begin(115200);
+  
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run(); // Initiates BlynkTimer
+}
